342359cc2135b21a34233136ff956a9369adb395
Micael Rosário <micaeldevrosario@gmail.com>
2024-08-29 20:00:22 -0300

    splash Screen

---
+++ AppMicael/app/src/main/java/com/example/appmicael/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.appmicael
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appmicael.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // Configuração do View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Ajuste de padding para system bars
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Handler para atrasar a mudança de atividade
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+}
